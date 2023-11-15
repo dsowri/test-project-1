073b69da0c9f57a5b3a64ce98e1dfdad652ff18e
dsowri <142078644+dsowri@users.noreply.github.com>
2023-11-15 14:02:57 +0530

    Update test-1.java

--- test-1.java
+++ test-1.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Jenkins and SonarQube!");
+        
+        // Simulate a change
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
     }
 }
